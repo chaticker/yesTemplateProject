960a54c973892ef02b5d4fca8d11b91504990a4f
youseonHwang <dbtjs0221@gmail.com>
2021-04-13 17:20:04 +0900

    [슨슨] package.json/tsconfig.json/MockDao.ts  경로 변경

--- server/src/daos/MockDb/MockDao.mock.ts
+++ server/src/daos/MockDb/MockDao.mock.ts
@@ -3,23 +3,23 @@ import { IUser } from '@entities/User';
 
 
 interface IDatabase {
-    users: IUser[];
+  users: IUser[];
 }
 
 
 class MockDaoMock {
 
-    private readonly dbFilePath = 'src/daos/MockDb/MockDb.json';
+  private readonly dbFilePath = 'server/src/daos/MockDb/MockDb.json';
 
 
-    protected openDb(): Promise<IDatabase> {
-        return jsonfile.readFile(this.dbFilePath) as Promise<IDatabase>;
-    }
+  protected openDb(): Promise<IDatabase> {
+    return jsonfile.readFile(this.dbFilePath) as Promise<IDatabase>;
+  }
 
 
-    protected saveDb(db: IDatabase): Promise<void> {
-        return jsonfile.writeFile(this.dbFilePath, db);
-    }
+  protected saveDb(db: IDatabase): Promise<void> {
+    return jsonfile.writeFile(this.dbFilePath, db);
+  }
 }
 
 export default MockDaoMock;
